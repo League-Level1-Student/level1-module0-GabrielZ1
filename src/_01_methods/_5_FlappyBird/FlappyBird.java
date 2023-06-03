69df4e30caaf61921d985774ea1b614cbe0b71b6
league <league@imac-16.attlocal.net>
2023-06-03 11:28:20 -0700

    FlappyBird and MyFirstSwingGUI

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,6 +1,7 @@
 package _01_methods._5_FlappyBird;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class FlappyBird extends PApplet {
 	static final int WIDTH = 800;
@@ -10,11 +11,23 @@ public class FlappyBird extends PApplet {
 	int birdYVelocity = -10;
 	int gravity = 1;
 
-	int pipeX = 700;
+	int pipeX = 800;
 
 	int upperPipeHeight = (int) random(100, 400);
 
+	int pipeWidth = 100;
+
 	int pipeGap = 150;
+
+	int lowerPipeTop;
+
+	boolean gameEnded = false;
+
+
+	int score = 0;
+
+	PImage back;
+	PImage bird;
 
 
 	@Override
@@ -24,47 +37,118 @@ public class FlappyBird extends PApplet {
 
 	@Override
 	public void setup() {
+		back = loadImage("flappyBackground.jpg");
+		back.resize(WIDTH, HEIGHT);
+
+		bird = loadImage("bird.png");
+		bird.resize(50,50);
+
 		teleportPipes();
 
 	}
 
 	@Override
 	public void draw() {
-		background(0, 255, 255);
-		fill(255, 255, 0);
-		stroke(255, 255, 0);
-		ellipse(posX,posY+birdYVelocity, 25, 25);
-		birdYVelocity += 3;
+		if(gameEnded) {
+			background(255,0,0);
 
-		stroke(0,0,0);
-		fill(0,255,0);
-		rect(pipeX,-1,100,upperPipeHeight);
+			fill(0,100,255);
+			textSize(50);
+			text("You lost.\nYour score was " + score + ".\nPress any key to restart.", 100, 200);		
 
-		int lowerY = upperPipeHeight + pipeGap;
 
-		rect(pipeX,lowerY,100,1000);
 
-		pipeX -= 5;
+			if(keyPressed) {
+				score = 0;
+				posY = 200;
+				pipeX = 700;
+				birdYVelocity = -10;
 
-		if(pipeX <= -100) {
-			teleportPipes();
+				upperPipeHeight = (int) random(100, 400);
+				
+				
+				gameEnded = false;	
+			}
+
 		}
 
+		else {
+			background(back);
+
+			fill(255,0,0);
+			textSize(25);
+			text("Score: " + score, WIDTH/2-100, HEIGHT/2-50);
+
+			fill(255, 255, 0);
+			stroke(255, 255, 0);
+			image (bird,posX,posY);
+			birdYVelocity += gravity;
+			posY += birdYVelocity;
+
+			stroke(0,0,0);
+			fill(0,255,0);
+			rect(pipeX,-1,pipeWidth,upperPipeHeight);
+
+			lowerPipeTop = upperPipeHeight + pipeGap;
+
+			rect(pipeX,lowerPipeTop,pipeWidth,1000);
+
+			pipeX -= 5;
+
+			if(pipeX <= -pipeWidth) {
+				teleportPipes();
+			}
+
+			fill(0,150,0);
+			rect(-5,HEIGHT-90,WIDTH+5,90);
+
+
+			if(intersectsPipes() || intersectsFloor()) {
+				gameEnded = true;
+			}
+
+		}
+
+		if(posX == (pipeX+pipeWidth)) {
+			score +=1;
+		}
 
 
 	}
 
 	public void mousePressed() {
-		birdYVelocity = 0;
+		birdYVelocity = -10;
 
 	}
 
 	public void teleportPipes() {
 		upperPipeHeight = (int) random(100, 400);
-		pipeX = 700;
+		pipeX = 800;
 
 	}
 
+
+	boolean intersectsPipes() { 
+		if (posY+10 < upperPipeHeight && posX+50 > pipeX && posX < (pipeX+pipeWidth)){
+			return true;
+		}
+		else if (posY+40 >lowerPipeTop && posX+50 > pipeX && posX < (pipeX+pipeWidth)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	boolean intersectsFloor() {
+		if(posY+35>HEIGHT-90) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
 
 	static public void main(String[] args) {
 		PApplet.main(FlappyBird.class.getName());
