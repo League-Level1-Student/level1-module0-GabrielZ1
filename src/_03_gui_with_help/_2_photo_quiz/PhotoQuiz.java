a162b7ae5caaba7a49b76a6f60e1bb94a45aae5e
league <league@localhost>
2023-06-10 11:32:06 -0700

    PhotoQuiz, DrumKit, BodyPartQuiz

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,34 +26,56 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+		String imageString1 = "https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component imageComp1;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		imageComp1 = createImage(imageString1);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(imageComp1);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String answer1 = JOptionPane.showInputDialog("What color is the cat's nose?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(answer1.equalsIgnoreCase("pink")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");	
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(imageComp1);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String imageString2 = "https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQkrjYxSfSHeCEA7hkPy8e2JphDsfFHZVKqx-3t37E4XKr-AT7DML8IwtwY0TnZsUcQ";
+		Component imageComp2 = createImage(imageString2);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(imageComp2);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String answer2 = JOptionPane.showInputDialog("What color is the dog's nose?");
 		// 14+ check answer, say if correct or incorrect, etc.
+		if(answer2.equalsIgnoreCase("black")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");	
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
+
+		
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.exit(0);
 
 	}
 
@@ -68,7 +90,7 @@ public class PhotoQuiz {
 			// TODO Auto-generated catch block
 			JOptionPane.showMessageDialog(null, "I can't find your image!!");
 		}
-		
+
 		return imageLabel;
 	}
 
